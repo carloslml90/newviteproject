2d2bfcec975c884bad7f45f831c9c316fe2bccba
Carlos <carloslml90@gmail.com>
2023-04-15 21:09:59 -0400

    first hello world

---
+++ src/Helloworld.jsx
@@ -0,0 +1,12 @@
+
+
+export const Hello =() => {
+
+return (
+
+    <h1>Hola mundo</h1>
+)
+
+
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { Hello } from './Helloworld'
+/* import './index.css' */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
 )
